6e687599a9167663e6971ad455b70c8d43851495
suptrung123 <78081087+suptrung123@users.noreply.github.com>
2021-10-09 22:05:11 -0500

    Add files via upload

--- bad_income.js
+++ bad_income.js
@@ -10,25 +10,25 @@ var calculateIncome = function ()
     var floatTaxRate, floatAnnualTax;
     if (intUserAge >= 65)
     {
-    	if (floatAnnualIncome =< 30000)
+    	if (floatAnnualIncome => 30000)
     	{
     		floatTaxRate = .025;
     	}
     	else
     	{
-    		if(floatAnnualIncome >= 30000 || floatAnnualIncome =< 75000)
+    		if(floatAnnualIncome >= 30000 || floatAnnualIncome <= 75000)
     		{
     			floatTaxRate = .05;
     		}
     		else
-    		{
+            {
     			floatTaxRate = .20;
     	}
     } 
     else
     {
-    	{
-    	if (floatAnnualIncome =< 30000)
+    	
+    	if (floatAnnualIncome => 30000)
     	{
     		floatTaxRate = .05;
     	}
@@ -51,12 +51,12 @@ var calculateIncome = function ()
 	alert ("Your annual income tax this year is $" + floatAnnualTax  +"\nBased upon an annual income of $"+ intAnnualIncome+
 	"\nAnd a tax rate of " +floatTaxRate+ "% \nNote: Tax rate is based upon your age");
 	
-}
+
 
 window.onload = function () 
 {
     $("annualIncome").value = "";
-    $("userAge").value = "";
+    $("userage").value = "";
     $("annualTax").value = "";
     $("calculate").onclick = calculateIncome;
     $("annualIncome").focus();
