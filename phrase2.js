b35d86dcc9c5c2b0c48bec6f25f635ead4c9243e
suptrung123 <78081087+suptrung123@users.noreply.github.com>
2021-12-01 13:48:45 -0600

    Add files via upload

--- phrase2.js
+++ phrase2.js
@@ -5,86 +5,88 @@ var $ = function (id)
 
 var vowels = function () 
 {
-	var stringEntry = $("phrase").value;
-	stringEntry = stringEntry.toUpperCase();
-	if (stringEntry==="")
-	{
-		$("phrase").value = "";
-		alert("Please enter a phrase and THEN select a button...");
-	}
-	else
-	{
-		var intVowels = 0;
-		//insert for loop here
-		for(intCount=0; intCount < stringEntry.length; intCount++)
+var stringEntry = $("phrase").value; 
+
+stringEntry = stringEntry.toUpperCase(); 
+
+if(stringEntry === "")
+{
+    alert("Please entry a phrase and then select the button"); 
+}
+else 
+{
+    var intVowels = 0; 
+    for(var intcount = 0; intcount < stringEntry.length; intcount++)
+    {
+        if(stringEntry.charAt(intcount) === "A" || stringEntry.charAt(intcount) === "E" || stringEntry.charAt(intcount) === "I" || stringEntry.charAt(intcount) === "O" || stringEntry.charAt(intcount) === "U")
         {
-            if(stringEntry.charAt (intCount)=== "A" || stringEntry.charAt (intCount)=== "E" || stringEntry.charAt (intCount)=== "I" ||stringEntry.charAt (intCount)=== "O" || stringEntry.charAt (intCount)=== "U" ||)
-        {
-            intVowels++;
+            intVowels++; 
         }
     }
-    
-    
-		$("output").value = "There are " + intVowels + " vowels in the above phrase.";
-	}	
-} 
+    $("output").value = "There are " + intVowels + "vowels in the above phrase."; 
+}
+
+}
+
 
 var consonants = function () 
 {
-	var stringEntry = $("phrase").value;
-	stringEntry = stringEntry.toLowerCase();
-	
-	if (stringEntry==="")
-	{
-		$("phrase").value = "";
-		alert("Please enter a phrase and THEN select a button...");
-	}
-	else
-	{
-		var intConsonants = 0;
-		// insert for loop here
-		for(intCount=0; intCount < stringEntry.length; intCount++)
-		{
-            switch(stringEntry.charAt(intCount))
-            {
-                case "a";
-                case "e";
-                case "i";
-                case "o";
-                case "u";
-                case " ";
-                      break;
-                default:
-                    intConsonants++;
-                    break;
-            }
-        }
-		
-		$("output").value = "There are " + intConsonants + " consonants in the above phrase.";
-	}
+    var stringEntry = $("phrase").value; 
+
+    stringEntry = stringEntry.toLowerCase(); 
+
+if (stringEntry === "")
+{
+    alert("Please entry a phrase and then select the button"); 
 }
+else 
+{
+    var intConsonants = 0; 
+    for(var intcount = 0; intcount < stringEntry.length; intcount++)
+    {
+        switch (stringEntry.charAt(intcount))
+        {
+            case "a":
+            case "e":
+            case "i": 
+            case "o":
+            case "u": 
+                break;
+            default:
+                intConsonants++;
+                break; 
+
+        }
+    }
+    $("output").value = "There are " + intConsonants + "Consonants in the above phrase."; 
+}
+
+
+}
+
+
 
 var reverse = function () 
 {
-	var stringEntry = $("phrase").value;
-	if (stringEntry==="")
-	{
-		$("phrase").value = "";
-		alert("Please enter a phrase and THEN select a button...");
-	}
-	else
-	{
-	
-		var stringReversed = "";
-		// insert for loop here
-		for (intCount=stringEntry.length-1; intCount>=0; intCount--)
+    var stringEntry = $("phrase").value; 
+    
+    if(stringEntry ==="")
+    {
+        alert("Please entry a phrase and then select the button"); 
+    }
+    else 
+    {
+        var stringReversed = ""; 
+        for(var intcount = stringEntry.length - 1; intcount >= 0; intcount--)
         {
-            stringReversed =stringReversed + stringEntry.charAt(intCount);
+            stringReversed = stringReversed + stringEntry.charAt(intcount); 
         }
-		
-		$("output").value = "The reverse of the above phrase is:\n" + stringReversed;
-	}
+        $("output").value = "The Reverse of the above phrase is: \n" + stringReversed; 
+    }
+    
 }
+
+
 
 var clear = function () 
 {
